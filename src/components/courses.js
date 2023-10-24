23df876a41370c79fc3aa058fff080d7794b6377
prasad khotkar <109893694+prasadkhotkar@users.noreply.github.com>
2023-10-24 18:23:39 +0530

    tailwind set in a project

---
+++ src/components/courses.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const courses = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default courses
---
+++ src/components/units.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const units = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default units
